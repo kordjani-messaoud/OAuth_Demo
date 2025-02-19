13e51d6d29e1b69b54c70ae460690e630b3c3ba0
KORDJANI Messaoud Nadjib <kordjanimessaoudnadjib@gmail.com>
2025-02-19 14:11:31 +0100

    ADD(auth): ValidAuthzReq, NewAuthzResCombinaison and RequestUserPermission to authorize.go

--- server/auth/authorize.go
+++ server/auth/authorize.go
@@ -1,34 +1,97 @@
 package auth
 
 import (
+	"crypto/rand"
+	"fmt"
 	"net/http"
 	"net/url"
+	"slices"
+	"strings"
 )
 
-func Authorize(w http.ResponseWriter, r *http.Request) {
+// I need to think more about the scopes
+var scopes []string
+var isScopesUpdated bool = false
 
+func Authorize(w http.ResponseWriter, r *http.Request) {
+	// Validate method
 	if r.Method != "GET" {
 		http.Error(w, "Invalide Method", http.StatusMethodNotAllowed)
-	}
+		return
 
+	}
+	// Validate querey params
 	if err := r.ParseForm(); err != nil {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
 	}
-
-	if params := r.Form; ValidateAuthReq(params) {
+	// Validate Auth req
+	if params := r.Form; ValidAuthzReq(params) {
+		clientId := ClientId(params["client_id"][0])
+		state := params["state"][0]
+		AutzResParams := NewAuthzResCombinaison(state)
+		AuthzResCombinaisons[clientId] = *AutzResParams
+		// Ask Resource Owner permission
+		RequestUserPermission(w, r, params)
 
 	} else {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 	}
 }
 
-func ValidateAuthReq(params url.Values) bool {
+// Validate Authorize requests
+// Returning bool
+// Get sent scopes and drop every one that is not on the registered scope list
+// Check if scopes had been update
+func ValidAuthzReq(params url.Values) bool {
+	// Get params
+	responseType := params["response_type"][0]
+	redirectURI := params["redirect_uri"][0]
+	clientId := ClientId(params["client_id"][0])
+	clientSecret := params["client_secret"][0]
+	scopes = params["scope"]
 
-	responseType := params["responseType"][0]
-	redirectURI := params["redirectURI"][0]
-	clientId := params["clientID"][0]
-	clientSecret := params["clientSecret"][0]
-	scopes = params["scopes"]
+	// Validate params
+	if app, present := apps[clientId]; present &&
+		redirectURI == app.RedirectURI &&
+		responseType == "code" &&
+		clientSecret == app.ClientSecret {
+		for _, scope := range scopes {
+			if !slices.Contains(app.Scopes, scope) {
+				scopes = slices.DeleteFunc(scopes, func(v string) bool {
+					return v == scope
+				})
+				isScopesUpdated = true
+			}
+		}
+		if len(scopes) != 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func NewAuthzResCombinaison(state string) *AuthzResCombinason {
+	return &AuthzResCombinason{
+		rand.Text(),
+		state,
+		rand.Text(),
+	}
+}
+
+// Q:Why do we parse the req form again couldn't we just ust the data stored. Res: Because the data is not stored
+func RequestUserPermission(w http.ResponseWriter, r *http.Request, params url.Values) {
+	clientId := ClientId(params["client_id"][0])
+	redirectURI := params["redirect_uri"][0]
 	state := params["state"][0]
 
+	url := fmt.Sprintf("%s/login?client_id=%s&redirect_uri=%s&scope=%s&state=%s",
+		SERVERURL,
+		clientId,
+		redirectURI,
+		strings.Join(scopes, " "),
+		state,
+	)
+
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
