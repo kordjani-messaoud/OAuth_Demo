8479be11ec0be8538384e4fcaa6311a6d7811835
KORDJANI Messaoud Nadjib <kordjanimessaoudnadjib@gmail.com>
2025-02-19 14:50:11 +0100

    FIX(client): Authorisation allways fails bug
     Correct parameter names in Submit handler function according to RFC

--- client/client.go
+++ client/client.go
@@ -20,7 +20,7 @@ var scopes []string
 func main() {
 
 	http.HandleFunc("/", root)
-	http.HandleFunc("/submit", submit)
+	http.HandleFunc("/submit", Submit)
 
 	fmt.Printf("Server Listening on port%s.\n", PORT)
 	err := http.ListenAndServe(PORT, nil)
@@ -41,17 +41,17 @@ func root(w http.ResponseWriter, r *http.Request) {
 	<body>
 	    <form action="/submit" method="GET">
 	        <input type="hidden" name="state" value="%s">
-	        <input type="hidden" name="redirectURI" value="%s">
-	        <input type="hidden" name="responseType" value="%s">
+	        <input type="hidden" name="redirect_uri" value="%s">
+	        <input type="hidden" name="response_type" value="%s">
 
 	        <label for="client_id">Client ID:</label>
-	        <input type="text" id="clientID" name="clientID" required><br><br>
+	        <input type="text" id="client_id" name="client_id" required><br><br>
 
 	        <label for="client_secret">Client Secret:</label>
-	        <input type="text" id="clientSecret" name="clientSecret" required><br><br>
+	        <input type="text" id="client_secret" name="client_secret" required><br><br>
 
 	        <label for="scopes">Scopes:</label>
-	        <input type="text" id="scopes" name="scopes" required><br><br>
+	        <input type="text" id="scope" name="scope" required><br><br>
 
 	        <button type="submit" name="action" value="allow">Submit</button>
 	    </form>
@@ -61,27 +61,29 @@ func root(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, html)
 }
 
-func submit(w http.ResponseWriter, r *http.Request) {
+func Submit(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != "GET" {
 		http.Error(w, "Invalide Method", http.StatusMethodNotAllowed)
+		return
 	}
 
 	if err := r.ParseForm(); err != nil {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
 	}
 
-	clientId = r.Form["clientID"][0]
-	clientSecret = r.Form["clientSecret"][0]
-	scopes = r.Form["scopes"]
-	responseType := r.Form["responseType"]
+	clientId = r.Form["client_id"][0]
+	clientSecret = r.Form["client_secret"][0]
+	scopes = r.Form["scope"]
+	responseType := r.Form["response_type"][0]
 
-	url := fmt.Sprintf("%s/authorize?response_type=%s&client_id=%s&client_secret=%s&redirect_uri=%sscope%s&state=%s",
+	url := fmt.Sprintf("%s/authorize?response_type=%s&client_id=%s&client_secret=%s&redirect_uri=%s&scope=%s&state=%s",
 		SERVERURL,
 		responseType,
 		clientId,
 		clientSecret,
-		CLIENTURL,
+		CLIENTURL+"/callback",
 		strings.Join(scopes, " "),
 		state,
 	)
