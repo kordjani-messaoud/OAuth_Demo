e6c5cb2566ff08edc2958cd54e28d190578488e6
KORDJANI Messaoud Nadjib <kordjanimessaoudnadjib@gmail.com>
2025-02-24 11:00:59 +0100

    ADD: 2 new route handlers to Server
     /token to answer Access Token requests
     /access to answer resource access requests

--- server/server.go
+++ server/server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"server/auth"
+	"server/resource"
 )
 
 const PORT string = ":8080"
@@ -15,8 +16,10 @@ func main() {
 	http.HandleFunc("/authorize", auth.Authorize)
 	http.HandleFunc("/login", auth.Login)
 	http.HandleFunc("/consent", auth.RequestApproval)
+	http.HandleFunc("/token", auth.GetAccessToken)
+	http.HandleFunc("/access", resource.DeliverData)
 
-	fmt.Println("Server Listening on port", PORT)
+	fmt.Printf("Server Listening on port%s\n", PORT)
 	err := http.ListenAndServe(PORT, nil)
 	log.Fatal(err)
 }
