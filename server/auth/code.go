2ba6c93fc90a4372056b84bbb6de1fcf69dbe477
KORDJANI Messaoud Nadjib <kordjanimessaoudnadjib@gmail.com>
2025-02-24 11:09:36 +0100

    ADD(auth): SendCode to Code.go and update ValidAccreditation signature

--- server/auth/code.go
+++ server/auth/code.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 func RequestApproval(w http.ResponseWriter, r *http.Request) {
@@ -17,22 +18,37 @@ func RequestApproval(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
 	}
+	params := r.Form
 
-	if params := r.form; ValidAccreditation(params)
+	if clientId, isValid := ValidAccreditation(params); isValid {
+		SendCode(w, r, clientId)
+	}
+
 	// Validate Auth req
 	fmt.Print(r.Form)
 }
 
-func ValidAccreditation(params url.Values) (ClientId, bool){
+func ValidAccreditation(params url.Values) (ClientId, bool) {
 
 	action := params["action"][0]
 	clientId := ClientId(params["client_id"][0])
 	state := params["state"][0]
 	storedState := AuthzResCombinaisons[clientId].State
 
-	if acction == "allow" &&
+	if action == "allow" &&
 		state == storedState {
-			return clientId, true
-		}
+		return clientId, true
+	}
 	return clientId, false
+}
+
+func SendCode(w http.ResponseWriter, r *http.Request, clientId ClientId) {
+
+	code := AuthzResCombinaisons[clientId].Code
+	state := AuthzResCombinaisons[clientId].State
+	redirectURI := apps[clientId].RedirectURI
+
+	url := fmt.Sprintf("%s?code=%s&state=%s", redirectURI, code, state)
+
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
