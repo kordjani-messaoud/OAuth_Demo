fd599655d68882f41459e75d6df636c2ebe8858f
KORDJANI Messaoud Nadjib <kordjanimessaoudnadjib@gmail.com>
2025-02-24 10:56:11 +0100

    ADD: new route handler to Client
    /callback allows Client to request Token from Server and access resources

--- client/client.go
+++ client/client.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -17,12 +21,18 @@ var clientSecret string
 var state string
 var scopes []string
 
+type Token struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+}
+
 func main() {
 
 	http.HandleFunc("/", root)
 	http.HandleFunc("/submit", Submit)
+	http.HandleFunc("/callback", callback)
 
-	fmt.Printf("Server Listening on port%s.\n", PORT)
+	fmt.Printf("Server Listening on port%s\n", PORT)
 	err := http.ListenAndServe(PORT, nil)
 	log.Fatal(err)
 }
@@ -89,4 +99,83 @@ func Submit(w http.ResponseWriter, r *http.Request) {
 	)
 
 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func callback(w http.ResponseWriter, r *http.Request) {
+
+	params := r.URL.Query()
+	token := GetAccessToken(w, params)
+	secretData := GetProtectedData(w, token)
+
+	html := fmt.Sprintf(`<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <title>POST Form</title>
+	</head>
+	<body>
+		<h1> Supper Secret Data </h1>
+		<b>%s</b>
+	</body>
+	</html>
+	`, secretData)
+	fmt.Fprint(w, html)
+}
+
+func GetAccessToken(w http.ResponseWriter, params url.Values) Token {
+
+	redirectURI := CLIENTURL + "/callback"
+	form := url.Values{
+		"grant_type":   []string{"authorization_code"},
+		"code":         params["code"],
+		"redirect_uri": []string{redirectURI},
+	}
+
+	uri := fmt.Sprintf("%s/token", SERVERURL)
+	req, _ := http.NewRequest("POST", uri, strings.NewReader(form.Encode()))
+
+	// req.SetBasicAuth(clientId, clientSecret)
+	clientCredentials := fmt.Sprintf("%s:%s", clientId, clientSecret)
+	clientCredentialsEncoded := base64.StdEncoding.EncodeToString([]byte(clientCredentials))
+	authzHeaderValue := fmt.Sprintf("Basic %s", clientCredentialsEncoded)
+	req.Header.Add("Authorization", authzHeaderValue)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+	}
+	resBody, _ := io.ReadAll(res.Body)
+
+	token := Token{}
+	json.Unmarshal(resBody, &token)
+
+	return token
+}
+
+func GetProtectedData(w http.ResponseWriter, token Token) string {
+	uri := fmt.Sprintf("%s/access?access_token=%s", SERVERURL, token.AccessToken)
+
+	req, err := http.NewRequest("GET", uri, nil)
+	if err != nil {
+		fmt.Println("Data Access Err", err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+	}
+
+	req.SetBasicAuth(clientId, clientSecret)
+
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("Data Access Err", err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+	}
+
+	resBody, _ := io.ReadAll(res.Body)
+
+	return string(resBody)
 }
